df1cebfd2cf148ad62638e1ebd80107f2aa7bd40
ChangJoo Park <pcjpcj2@gmail.com>
2016-12-24 17:06:49 +0900

    Initialize Commit

--- renderer.js
+++ renderer.js
@@ -1,3 +1,45 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const app = new Vue({
+  el: '#app',
+  created: function () {
+    this.state = 'list'
+  },
+  data: function () {
+    return {
+      state: '',
+      repositories: [],
+      newRepository: '',
+      currentIndex: -1
+    }
+  },
+  methods: {
+    changeState: function (nextState, repoIndex) {
+      if (nextState === 'edit' && repoIndex !== -1) {
+        this.currentIndex = repoIndex
+      }
+      this.state = nextState;
+    },
+    addRepository: function () {
+      var newRepo = this.newRepository.trim();
+      if(!newRepo)  {
+        console.log('empty')
+        return;
+      }
+      this.repositories.push(newRepo)
+      this.changeState('list')
+    },
+    updateRepository: function () {
+      this.backToList();
+    },
+    removeRepository: function () {
+      this.repositories.splice(this.currentIndex, 1);
+      this.backToList();
+    },
+    backToList: function () {
+      this.changeState('list');
+      this.currentIndex = -1;
+    }
+  }
+});
